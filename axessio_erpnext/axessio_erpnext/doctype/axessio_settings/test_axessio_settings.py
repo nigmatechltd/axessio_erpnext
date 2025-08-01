befcb085b583d1d27a3b9b632ed0333b0cf1badb
2arry <larryadah@gmail.com>
2025-08-01 09:04:09 +0000

    Axessio Settings for Purchase Order Email

---
+++ axessio_erpnext/axessio_erpnext/doctype/axessio_settings/test_axessio_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2025, Nigma Tech Limited and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestAxessioSettings(FrappeTestCase):
+	pass
